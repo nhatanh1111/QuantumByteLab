8c05c1dc54cab29e7477420fc5c9b07a721219ce
nhatanh1111 <spotjordanpun10suy11@hotmail.com>
2024-05-07 13:36:49 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const fibonacci = (n) => {
-  const sequence = [0, 1];
-  for (let i = 2; i <= n; i++) {
-    sequence.push(sequence[i - 1] + sequence[i - 2]);
-  }
-  return sequence;
-};
+const flatten = (arr) => arr.reduce((a, v) => a.concat(v), []);
