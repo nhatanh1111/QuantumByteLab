e3df0f2e4761d93323dc9e0677fb983a47b86471
nhatanh1111 <spotjordanpun10suy11@hotmail.com>
2024-05-13 10:18:54 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-function rob(nums) {
-  let prevMax = 0;
-  let currMax = 0;
-  for (const num of nums) {
-    const temp = currMax;
-    currMax = Math.max(prevMax + num, currMax);
-    prevMax = temp;
+function searchRange(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let start = -1;
+  let end = -1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      start = mid;
+      end = mid;
+      while (nums[start] === target) start--;
+      while (nums[end] === target) end++;
+      return [start + 1, end - 1];
+    } else if (nums[mid] < target) left = mid + 1;
+    else right = mid - 1;
   }
-  return currMax;
+  return [start, end];
 }
